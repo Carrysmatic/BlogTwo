b4fc237b8f516b6ce4f78e5db4fa9de712fc9d36
Stefan <stefan.plopianu@gmail.com>
2023-04-19 15:18:40 +0300

    Created grid + main bar

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,44 +8,293 @@ export default function Home() {
       <div className={styles.mainBar}>
         <div className={styles.mainBarContent}>
           <div className={styles.mainBarContentLeft}>
-            {/* searchbar */}
+            <form className={styles.form}>
+              <input type="search" className={styles.formControl} placeholder="Search..." />
+            </form>
           </div>
           <div className={styles.mainBarContentMiddle}>
-            {/* title of page*/}
+            <h1>MAGDESIGN</h1>
           </div>
           <div className={styles.mainBarContentRight}>
-            {/* icons */}
+            <ul className={styles.mainBarContentRightList}>
+              <li>
+                <a href="#">T</a>
+              </li>
+              <li>
+                <a href="#">I</a>
+              </li>
+              <li>
+                <a href="#">F</a>
+              </li>
+            </ul>
           </div>
         </div>
-        </div>
-        <div className={styles.firstTitle}>
-          <h1>My Title</h1>
-        </div>
-        <div className={styles.firstCarousel}>
-          {/* carousel */}
-        </div>
-        <div className={styles.firstContent}>
-          <div className={styles.firstContentBox}>
-            {/* content */}
-          </div>
-        </div>
-        <div className={styles.secondTitle}>
-          <h1>My Title</h1>
-        </div>
-        <div className={styles.secondCarousel}>
-          {/* carousel */}
-        </div>
-        <div className={styles.secondContent}>
-          <div className={styles.secondContentBox}>
-            {/* content */}
-          </div>
-        </div>
-        <div className={styles.subscribe}>
-          {/* subscribe to newsletter  */}
-          </div>
-          <div className={styles.footer}>
-            {/* footer */}
+      </div>
+
+      <div className={styles.firstTitle}>
+        <h1>Trending</h1>
+      </div>
+      <div className={styles.firstCarousel}>
+        {/* carousel */}
+      </div>
+      <div className={styles.firstContent}>
+      <div className={styles.firstContentBox}>
+          <img src="https://preview.colorlib.com/theme/magdesign/images/img_2.jpg" alt="" />
+          <div className={styles.firstContentBoxCategory}>
+            <div className={styles.firstContentBoxCategoryBlog}>
+              <p>Business, Travel</p>
             </div>
+            <div className={styles.firstContentBoxCategoryDate}>
+              <p>- May 28,2021</p>
+            </div>
+          </div>
+          <h2>Your most unhappy customers are your greatest source of learning.</h2>
+          <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+          <div className={styles.firstContentBoxAuthor}>
+            <img src="https://preview.colorlib.com/theme/magdesign/images/person_1.jpg" alt="" />
+            <div className={styles.firstContentBoxAuthorName}>
+              <strong>Sergy Campbell</strong>
+              <span> CEO and Founder</span>
+            </div>
+          </div>
+        </div>
+        <div className={styles.firstContentBox}>
+          <img src="https://preview.colorlib.com/theme/magdesign/images/img_2.jpg" alt="" />
+          <div className={styles.firstContentBoxCategory}>
+            <div className={styles.firstContentBoxCategoryBlog}>
+              <p>Business, Travel</p>
+            </div>
+            <div className={styles.firstContentBoxCategoryDate}>
+              <p>- May 28,2021</p>
+            </div>
+          </div>
+          <h2>Your most unhappy customers are your greatest source of learning.</h2>
+          <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+          <div className={styles.firstContentBoxAuthor}>
+            <img src="https://preview.colorlib.com/theme/magdesign/images/person_1.jpg" alt="" />
+            <div className={styles.firstContentBoxAuthorName}>
+              <strong>Sergy Campbell</strong>
+              <span> CEO and Founder</span>
+            </div>
+          </div>
+        </div>
+        <div className={styles.firstContentBox}>
+          <img src="https://preview.colorlib.com/theme/magdesign/images/img_2.jpg" alt="" />
+          <div className={styles.firstContentBoxCategory}>
+            <div className={styles.firstContentBoxCategoryBlog}>
+              <p>Business, Travel</p>
+            </div>
+            <div className={styles.firstContentBoxCategoryDate}>
+              <p>- May 28,2021</p>
+            </div>
+          </div>
+          <h2>Your most unhappy customers are your greatest source of learning.</h2>
+          <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+          <div className={styles.firstContentBoxAuthor}>
+            <img src="https://preview.colorlib.com/theme/magdesign/images/person_1.jpg" alt="" />
+            <div className={styles.firstContentBoxAuthorName}>
+              <strong>Sergy Campbell</strong>
+              <span> CEO and Founder</span>
+            </div>
+          </div>
+        </div>
+        <div className={styles.firstContentBox}>
+          <img src="https://preview.colorlib.com/theme/magdesign/images/img_2.jpg" alt="" />
+          <div className={styles.firstContentBoxCategory}>
+            <div className={styles.firstContentBoxCategoryBlog}>
+              <p>Business, Travel</p>
+            </div>
+            <div className={styles.firstContentBoxCategoryDate}>
+              <p>- May 28,2021</p>
+            </div>
+          </div>
+          <h2>Your most unhappy customers are your greatest source of learning.</h2>
+          <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+          <div className={styles.firstContentBoxAuthor}>
+            <img src="https://preview.colorlib.com/theme/magdesign/images/person_1.jpg" alt="" />
+            <div className={styles.firstContentBoxAuthorName}>
+              <strong>Sergy Campbell</strong>
+              <span> CEO and Founder</span>
+            </div>
+          </div>
+        </div>
+        <div className={styles.firstContentBox}>
+          <img src="https://preview.colorlib.com/theme/magdesign/images/img_2.jpg" alt="" />
+          <div className={styles.firstContentBoxCategory}>
+            <div className={styles.firstContentBoxCategoryBlog}>
+              <p>Business, Travel</p>
+            </div>
+            <div className={styles.firstContentBoxCategoryDate}>
+              <p>- May 28,2021</p>
+            </div>
+          </div>
+          <h2>Your most unhappy customers are your greatest source of learning.</h2>
+          <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+          <div className={styles.firstContentBoxAuthor}>
+            <img src="https://preview.colorlib.com/theme/magdesign/images/person_1.jpg" alt="" />
+            <div className={styles.firstContentBoxAuthorName}>
+              <strong>Sergy Campbell</strong>
+              <span> CEO and Founder</span>
+            </div>
+          </div>
+        </div>
+        <div className={styles.firstContentBox}>
+          <img src="https://preview.colorlib.com/theme/magdesign/images/img_2.jpg" alt="" />
+          <div className={styles.firstContentBoxCategory}>
+            <div className={styles.firstContentBoxCategoryBlog}>
+              <p>Business, Travel</p>
+            </div>
+            <div className={styles.firstContentBoxCategoryDate}>
+              <p>- May 28,2021</p>
+            </div>
+          </div>
+          <h2>Your most unhappy customers are your greatest source of learning.</h2>
+          <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+          <div className={styles.firstContentBoxAuthor}>
+            <img src="https://preview.colorlib.com/theme/magdesign/images/person_1.jpg" alt="" />
+            <div className={styles.firstContentBoxAuthorName}>
+              <strong>Sergy Campbell</strong>
+              <span> CEO and Founder</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.secondTitle}>
+        <h1>Most Popular Posts</h1>
+      </div>
+      <div className={styles.secondCarousel}>
+        {/* carousel */}
+      </div>
+      <div className={styles.firstContent}>
+      <div className={styles.firstContentBox}>
+          <img src="https://preview.colorlib.com/theme/magdesign/images/img_2.jpg" alt="" />
+          <div className={styles.firstContentBoxCategory}>
+            <div className={styles.firstContentBoxCategoryBlog}>
+              <p>Business, Travel</p>
+            </div>
+            <div className={styles.firstContentBoxCategoryDate}>
+              <p>- May 28,2021</p>
+            </div>
+          </div>
+          <h2>Your most unhappy customers are your greatest source of learning.</h2>
+          <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+          <div className={styles.firstContentBoxAuthor}>
+            <img src="https://preview.colorlib.com/theme/magdesign/images/person_1.jpg" alt="" />
+            <div className={styles.firstContentBoxAuthorName}>
+              <strong>Sergy Campbell</strong>
+              <span> CEO and Founder</span>
+            </div>
+          </div>
+        </div>
+        <div className={styles.firstContentBox}>
+          <img src="https://preview.colorlib.com/theme/magdesign/images/img_2.jpg" alt="" />
+          <div className={styles.firstContentBoxCategory}>
+            <div className={styles.firstContentBoxCategoryBlog}>
+              <p>Business, Travel</p>
+            </div>
+            <div className={styles.firstContentBoxCategoryDate}>
+              <p>- May 28,2021</p>
+            </div>
+          </div>
+          <h2>Your most unhappy customers are your greatest source of learning.</h2>
+          <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+          <div className={styles.firstContentBoxAuthor}>
+            <img src="https://preview.colorlib.com/theme/magdesign/images/person_1.jpg" alt="" />
+            <div className={styles.firstContentBoxAuthorName}>
+              <strong>Sergy Campbell</strong>
+              <span> CEO and Founder</span>
+            </div>
+          </div>
+        </div>
+        <div className={styles.firstContentBox}>
+          <img src="https://preview.colorlib.com/theme/magdesign/images/img_2.jpg" alt="" />
+          <div className={styles.firstContentBoxCategory}>
+            <div className={styles.firstContentBoxCategoryBlog}>
+              <p>Business, Travel</p>
+            </div>
+            <div className={styles.firstContentBoxCategoryDate}>
+              <p>- May 28,2021</p>
+            </div>
+          </div>
+          <h2>Your most unhappy customers are your greatest source of learning.</h2>
+          <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+          <div className={styles.firstContentBoxAuthor}>
+            <img src="https://preview.colorlib.com/theme/magdesign/images/person_1.jpg" alt="" />
+            <div className={styles.firstContentBoxAuthorName}>
+              <strong>Sergy Campbell</strong>
+              <span> CEO and Founder</span>
+            </div>
+          </div>
+        </div>
+        <div className={styles.firstContentBox}>
+          <img src="https://preview.colorlib.com/theme/magdesign/images/img_2.jpg" alt="" />
+          <div className={styles.firstContentBoxCategory}>
+            <div className={styles.firstContentBoxCategoryBlog}>
+              <p>Business, Travel</p>
+            </div>
+            <div className={styles.firstContentBoxCategoryDate}>
+              <p>- May 28,2021</p>
+            </div>
+          </div>
+          <h2>Your most unhappy customers are your greatest source of learning.</h2>
+          <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+          <div className={styles.firstContentBoxAuthor}>
+            <img src="https://preview.colorlib.com/theme/magdesign/images/person_1.jpg" alt="" />
+            <div className={styles.firstContentBoxAuthorName}>
+              <strong>Sergy Campbell</strong>
+              <span> CEO and Founder</span>
+            </div>
+          </div>
+        </div>
+        <div className={styles.firstContentBox}>
+          <img src="https://preview.colorlib.com/theme/magdesign/images/img_2.jpg" alt="" />
+          <div className={styles.firstContentBoxCategory}>
+            <div className={styles.firstContentBoxCategoryBlog}>
+              <p>Business, Travel</p>
+            </div>
+            <div className={styles.firstContentBoxCategoryDate}>
+              <p>- May 28,2021</p>
+            </div>
+          </div>
+          <h2>Your most unhappy customers are your greatest source of learning.</h2>
+          <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+          <div className={styles.firstContentBoxAuthor}>
+            <img src="https://preview.colorlib.com/theme/magdesign/images/person_1.jpg" alt="" />
+            <div className={styles.firstContentBoxAuthorName}>
+              <strong>Sergy Campbell</strong>
+              <span> CEO and Founder</span>
+            </div>
+          </div>
+        </div>
+        <div className={styles.firstContentBox}>
+          <img src="https://preview.colorlib.com/theme/magdesign/images/img_2.jpg" alt="" />
+          <div className={styles.firstContentBoxCategory}>
+            <div className={styles.firstContentBoxCategoryBlog}>
+              <p>Business, Travel</p>
+            </div>
+            <div className={styles.firstContentBoxCategoryDate}>
+              <p>- May 28,2021</p>
+            </div>
+          </div>
+          <h2>Your most unhappy customers are your greatest source of learning.</h2>
+          <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+          <div className={styles.firstContentBoxAuthor}>
+            <img src="https://preview.colorlib.com/theme/magdesign/images/person_1.jpg" alt="" />
+            <div className={styles.firstContentBoxAuthorName}>
+              <strong>Sergy Campbell</strong>
+              <span> CEO and Founder</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.subscribe}>
+        {/* subscribe to newsletter  */}
+      </div>
+      <div className={styles.footer}>
+        {/* footer */}
+      </div>
     </div>
 
   )
